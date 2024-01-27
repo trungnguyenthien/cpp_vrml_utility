9c8a73220cb85c488e32d5fb4741904b96b52d2f
Trung Nguyen Thien <ngthientrung@gmail.com>
2024-01-28 06:34:08 +0700

    No install macport script

--- _install_mac.sh
+++ _install_mac.sh
@@ -1,9 +1,2 @@
-PORT_VERSION='2.9.0'
-cd /usr
-sudo curl -O https://distfiles.macports.org/MacPorts/MacPorts-$PORT_VERSION.tar.bz2
-sudo tar xjvf MacPorts-$PORT_VERSION.tar.bz2
-cd MacPorts-$PORT_VERSION
-./configure && make && sudo make install
-export PATH=/opt/local/bin:/opt/local/sbin:$PATH
 sudo port -v selfupdate
 sudo port install cairo
