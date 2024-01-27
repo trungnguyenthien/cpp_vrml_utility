afb16ac74e9a6c3f02227604a63e308747d447ba
Trung Nguyen Thien <ngthientrung@gmail.com>
2024-01-27 19:50:21 +0700

    Update gitignore

--- _build_mac.sh
+++ _build_mac.sh
@@ -1,4 +1,5 @@
 clean
 cmake .
 cmake --build .
-dist/g4_gamoslog_to_cubic --log data/gamos.log --wrl data/g4_01.wrl
+# dist/g4_gamoslog_to_cubic --log data/gamos.log --wrl data/g4_01.wrl
+dist/rand_cube_in_vrml -f "gamma.wrl" -n 1000 -s 1
