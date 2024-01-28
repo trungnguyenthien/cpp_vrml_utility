85382c77544ed739661c1a55db514bb19e649c7a
Trung Nguyen Thien <ngthientrung@gmail.com>
2024-01-28 08:49:05 +0700

    Add library

--- _install_mac.sh
+++ _install_mac.sh
@@ -1,3 +1,6 @@
 git clone https://github.com/trungnguyenthien/CLI11 modules/cli11
 git clone https://github.com/trungnguyenthien/geos modules/geos
-git clone https://github.com/trungnguyenthien/cgal modules/cgal
+brew install boost
+brew install gmp # --> cgal 
+brew install mpfr # --> cgal 
+brew install cgal
