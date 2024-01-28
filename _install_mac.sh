51374bd2ec08aa6b1ed9cbc87492f28767debe9f
Trung Nguyen Thien <ngthientrung@gmail.com>
2024-01-28 07:10:34 +0700

    Update

--- _install_mac.sh
+++ _install_mac.sh
@@ -1,2 +1,3 @@
-git clone https://github.com/CLIUtils/CLI11 modules/cli11
-git clone https://github.com/CGAL/cgal modules/cgal
+git clone https://github.com/trungnguyenthien/CLI11 modules/cli11
+git clone https://github.com/trungnguyenthien/geos modules/geos
+git clone https://github.com/trungnguyenthien/cgal modules/cgal
